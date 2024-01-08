b545cf425b77966079fe717e5daf74d2e8d2c67d
Kaiqueav <kaiquecerq66@outlook.com>
2024-01-07 22:28:32 -0300

    criando processo seletivo

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,16 +1,20 @@
-public class App {
+public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        System.out.println("Processso seletivo");
+       analisarCanditado(1900.0);
+       analisarCanditado(2200.0);
+       analisarCanditado(2000.0);
 
 
     }
 
-    static void analisarCanaditado( double salarioEstimado){
+    static void analisarCanditado( double salarioEstimado){
         double salarioBase = 2000.0;
         if (salarioBase > salarioEstimado) { 
             System.out.println("Ligar para candidato");
         }else if (salarioBase == salarioEstimado){
             System.out.println("aguardando");
+        } else { 
+            System.out.println("aguardando demais");
         }
         
        
